cebe3c5a8205cc3ed52feb3938f85e15f2cf4b65
Nikola Marcetic <n.marcetic86@gmail.com>
2021-11-18 14:06:41 +0100

    Fixed mongo init path
    Signed-off-by: Nikola Marcetic <n.marcetic86@gmail.com>

--- docker/scripts/mongo-init.sh
+++ docker/scripts/mongo-init.sh
@@ -1,3 +1,3 @@
 mongo --eval "rs.initiate()"
 # import exchange db (takes around 10-15 mins)
-mongorestore --uri="mongodb://localhost:27017"  --gzip --archive=mongo/exchangedb --nsInclude=exchange.* -v
+mongorestore --uri="mongodb://localhost:27017"  --gzip --archive=exchangedb --nsInclude=exchange.* -v
