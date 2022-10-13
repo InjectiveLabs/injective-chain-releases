eac106f7554eda239ffb3cc3b82a3ad4c281052d
Ri-go <rigo@paymongo.com>
2022-10-13 15:48:52 +0800

    fix: wait before write

--- docker/scripts/mongo-init.sh
+++ docker/scripts/mongo-init.sh
@@ -1,3 +1,12 @@
-mongo --eval "rs.initiate()"
+mongosh --eval "rs.initiate()"
+
+# Wait for primary replica to be ready before performing create commands. Restoring from backup will fail otherwise.
+echo "Waiting for Mongo to start"
+until mongosh --eval "rs.status()" | grep "stateStr: 'PRIMARY'"; do
+    echo "Still waiting for Mongo to start"
+    sleep 5
+done
+echo "Mongo started"
+
 # import exchange db (takes around 10-15 mins)
-mongorestore --uri="mongodb://localhost:27017"  --gzip --archive=exchangedb --nsInclude=exchange.* -v
+mongorestore --uri="mongodb://localhost:27017" --gzip --archive=exchangeV2 --nsInclude=exchangeV2.* -v
